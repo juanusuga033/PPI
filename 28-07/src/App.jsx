f577d97d5143c1d206c6330b60fcfb81e0ef4372
juanusuga033 <juanusugam@ielacandelariamedellin.edu.co>
2026-07-30 21:08:30 +0000

    mensaje

--- 28-07/src/App.jsx
+++ 28-07/src/App.jsx
@@ -13,7 +13,7 @@ function App() {
               <Link to="/">Inicio</Link>
             </li>
             <li>
-              {session ? <Link to="/profile">Mi Perfil</Link> : <Link to="/login">Iniciar sesión</Link>}
+             <Link to="/login">Iniciar sesión</Link>
             </li>
             <li>
               <Link to="/register">Registro</Link>
